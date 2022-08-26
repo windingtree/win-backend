fd2b38a00f79fb4be3d619d2da0a311cb37ece51
Eugene <galeaf11@gmail.com>
2022-08-26 08:39:07 +0600

    feat: improvement of indexes

--- _docker/mongod/scripts/js/collections/04_hotels.js
+++ _docker/mongod/scripts/js/collections/04_hotels.js
@@ -2,4 +2,4 @@ db.createCollection('hotels');
 db.hotels.createIndex({ location: '2dsphere' });
 db.hotels.createIndex({ hotelId: 1 });
 db.hotels.createIndex({ provider: 1 });
-db.hotels.createIndex({ createdAt: 1 }, { expireAfterSeconds: 12 * 60 * 60 }); //12h
+db.hotels.createIndex({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 }); //1h
--- _docker/mongod/scripts/js/collections/06_deals.js
+++ _docker/mongod/scripts/js/collections/06_deals.js
@@ -1,3 +1,5 @@
 db.createCollection('deals');
 db.deals.createIndex({ userAddress: 1 });
 db.deals.createIndex({ offerId: 1 });
+db.deals.createIndex({ orderId: 1 });
+db.deals.createIndex({ supplierReservationId: 1 });
