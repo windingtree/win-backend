e95e6af577347dddbf48a3e5849309c952613f10
Eugene <galeaf11@gmail.com>
2022-09-19 11:49:47 +0600

    fix: index naming

--- _docker/mongod/scripts/js/collections/08_user_requests.js
+++ _docker/mongod/scripts/js/collections/08_user_requests.js
@@ -1,5 +1,8 @@
-db.createCollection('sessions');
-db.deals.createIndex({ accommodationId: 1 });
-db.deals.createIndex({ requestHash: 1 });
-db.deals.createIndex({ sessionId: 1 });
-db.deals.createIndex({ startDate: 1 }, { expireAfterSeconds: 60 * 60 * 24 }); //will delete after 1 day
+db.createCollection('user_requests');
+db.user_requests.createIndex({ accommodationId: 1 });
+db.user_requests.createIndex({ requestHash: 1 });
+db.user_requests.createIndex({ sessionId: 1 });
+db.user_requests.createIndex(
+  { startDate: 1 },
+  { expireAfterSeconds: 60 * 60 * 24 }
+); //will delete after 1 day
